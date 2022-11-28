ba8baac25686d84f691613912bfe401a595ac4db
Nikhil Sharma <nikhilsharma230303@gmail.com>
2022-11-29 00:59:15 +0530

    add task 2: interface

---
+++ interface/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type ProfilePicture struct {
+	Name string
+	Path string
+}
+
+type Profile struct {
+	Name          string
+	Username      string
+	Designation   string
+	ContactNumber string
+	ProfilePicture
+}
+
+type ProfileMaker interface {
+	UpdateProfilePicture(ProfilePicture)
+	CheckDuplicateProfile(Profile) bool
+}
+
+func main() {
+	profile := GetProfile("Nikhil", "NikhilSharmaWe", "Product Engineer", "xxxcc", "vvv/dfe/rr.jpg", "999999999")
+	fmt.Println(profile)
+	modifiedProfile := GetProfile("Sharma", "NikhilSharmaWe", "Product Engineer", "xxxcc", "vvv/dfe/rr.jpg", "999999999")
+	profile.UpdateProfile(modifiedProfile)
+	fmt.Println(profile)
+}
+
+func GetProfile(name, username, designation, imageName, imagePath, contactNumber string) Profile {
+	profile := Profile{
+		Name:          name,
+		Username:      username,
+		Designation:   designation,
+		ContactNumber: contactNumber,
+		ProfilePicture: ProfilePicture{
+			Name: imageName,
+			Path: imagePath,
+		},
+	}
+	return profile
+}
+
+func (profile *Profile) UpdateProfile(modifiedProfile Profile) {
+	*profile = modifiedProfile
+}
+
+func (picture *ProfilePicture) UpdateProfilePicture(modifiedProfilePicture ProfilePicture) {
+	*picture = modifiedProfilePicture
+}
+
+func (profile Profile) CheckDuplicateProfile(profileToBeChecked Profile) bool {
+	return (profile == profileToBeChecked)
+}
